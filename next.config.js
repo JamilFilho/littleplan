1ee4ed77f4e83375eb99027110cd128b05d569a5
Jamil Filho <jamilf.alien@mgmail.com>
2022-12-22 18:53:27 -0300

    fix: resolução erro bad-precaching-response

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
 module.exports = withPWA({
+  pwa: {
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/]
+  },
   images: {
     domains: ['s3-us-west-2.amazonaws.com', 'drive.littleson.com.br', 'host.littleson.com.br']
   }
