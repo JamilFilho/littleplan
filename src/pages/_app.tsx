9e780b75779587318758650e12d0f89d14b8dc42
Jamil Filho <jamilf.alien@mgmail.com>
2022-12-22 17:08:56 -0300

    fix: correção o meta viewport
    Transferência da metatag do _app.js para o _documents.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/global.scss'
-import Head from 'next/head'
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
 import styles from '../styles/app.module.scss'
@@ -8,9 +7,6 @@ import { PlayerContextProvider } from '../contexts/PlayerContext'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'/>
-    </Head>
     <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
