0e92bc02494ae95d06b8afa05941dab65c5c0c95
Jamil Filho <jamilf.alien@mgmail.com>
2022-12-23 16:30:30 -0300

    fix: corrigindo API_KEY_LYKET no config.next.js

--- next.config.js
+++ next.config.js
@@ -15,5 +15,8 @@ module.exports = withPWA({
   },
   images: {
     domains: ['s3-us-west-2.amazonaws.com', 'drive.littleson.com.br', 'host.littleson.com.br']
+  },
+  env: {
+    API_KEY_LYKET: process.env.API_KEY_LYKET,
   }
 })
