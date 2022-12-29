736e99243b6ead01f2a0dbdc31b6bbd8635e4668
Jamil Filho <jamilf.alien@mgmail.com>
2022-12-29 18:26:13 -0300

    chore: atualização de pacotes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
-module.exports = withPWA({
+module.exports = {
   images: {
-    domains: ['s3-us-west-2.amazonaws.com', 'drive.littleson.com.br', 'host.littleson.com.br']
+    domains: ['drive.littleson.com.br', 'host.littleson.com.br']
   },
   env: {
     API_KEY_LYKET: process.env.API_KEY_LYKET,
   }
-})
+}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://raw.githubusercontent.com/JamilFilho/littleplan/main/db.json"
+  baseURL: process.env.API_URL || "https://my-json-server.typicode.com/JamilFilho/littleplan"
 })
