d0f38902a94e7740eac944bbdf73e35123578e97
Jamil Filho <jamilf.alien@mgmail.com>
2022-12-31 16:11:44 -0300

    fix: correção url
    Correção da url nos botões de compartilhamento e na metatag og

--- src/components/Sharer/index.js
+++ src/components/Sharer/index.js
@@ -6,15 +6,15 @@ export function ShareButtons({title, url}) {
     useScript('/assets/sharer.min.js')
     return(
         <section className={styles.sharerButtons}>
-            <button data-sharer="facebook" data-title={title} data-url={`https://plan.littleson.com.br/${url}`} className={styles.shareButton}>
+            <button data-sharer="facebook" data-title={title} data-url={`https://plan.littleson.com.br/leitura/${url}`} className={styles.shareButton}>
                 <FaFacebook />
             </button>
             
-            <button data-sharer="twitter" data-title={title} data-via="um_littleson" data-url={`https://plan.littleson.com.br/${url}`} className={styles.shareButton}>
+            <button data-sharer="twitter" data-title={title} data-via="um_littleson" data-url={`https://plan.littleson.com.br/leitura/${url}`} className={styles.shareButton}>
                 <FaTwitter />
             </button>
             
-            <button data-sharer="whatsapp" data-title={title} data-url={`https://plan.littleson.com.br/${url}`} className={styles.shareButton}>
+            <button data-sharer="whatsapp" data-title={title} data-url={`https://plan.littleson.com.br/leitura/${url}`} className={styles.shareButton}>
                 <FaWhatsapp />
             </button>
         </section>
